592fb8e36daabcbfc9b8f955ce355c186229910e
alexg84 <galperin25@gmail.com>
2018-06-21 15:27:49 +0300

    Update nfsclient.sh

--- nfsclient.sh
+++ nfsclient.sh
@@ -5,9 +5,9 @@ sudo yum install nfs-utils -y
 sudo mkdir -p /mnt/nfs/home
 sudo mkdir -p /mnt/nfs/var/nfsshare
 
-sudo mount -t nfs 10.128.0.9:/home /mnt/nfs/home/
+sudo mount -t nfs <server nfs ip>:/home /mnt/nfs/home/
 
-sudo mount -t nfs 10.128.0.9:/var/nfsshare /mnt/nfs/var/nfsshare/
+sudo mount -t nfs <server nfs ip>:/var/nfsshare /mnt/nfs/var/nfsshare/
 
 
 
